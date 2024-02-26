eb44021925cb62b68ccc7589b5cb0379ebaa3eb4
Tyler Wagner <tylerrwagner38@gmail.com>
2024-02-26 09:55:14 -0600

    created Config.py
    All documentation for this is located in the file itself

---
+++ Backend/config.py
@@ -0,0 +1,15 @@
+import configparser
+
+# I need to work on this at some point in the near future
+# all this will do is allow us to read and create config files
+# specific to each user
+# might need to call this in the settings area but im not 100% sure
+# This will be a WIP for a while honestly
+
+
+# This class will allow the users to upload their own custom config files
+# there needs to be clear documentation on this however for the user
+
+class Config:
+    def __init__(self) -> None:
+        pass
