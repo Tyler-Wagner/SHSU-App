7fdf2fccdc4b3e3d6959339f7c3b928d234f56d7
P3akyB0y <Broussard0710@gmail.com>
2024-04-15 10:17:49 -0500

    Major Push after connecting dashboard to tabs
    various updates for dashboard and tabs as well as connecting the backend

--- IntruWatch.py
+++ IntruWatch.py
@@ -3,6 +3,7 @@ import psutil
 # from Backedn.tcp_handler import handle_tcp_packet
 # from Backedn.udp_handler import handle_udp_packet
 import threading
+<<<<<<< Updated upstream
 from Handlers.enterData import  EnterDataHandler
 from PyQt5.QtWidgets import QApplication, QMainWindow
 # from PyQt5.QtCore import QTimer
@@ -10,6 +11,17 @@ from Frontend.tabs import Ui_tabsPage
 # from time import sleep # FOR SIMULATION
 from Backend.main import list_network_devices
 from Backend.main import capture_packets
+=======
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from Frontend.ui_Dashboard import Ui_Dashboard, RealTimeGraph
+from Frontend.Ui_Tabspage import Ui_TabsPage
+from Handlers.enterData import EnterDataHandler
+from main import list_network_devices, capture_packets
+from Handlers.dbHandle import importUserSettings as dbhandle_SETTINGS
+from Handlers.dbHandle import importPastAlerts as getPastAlerts
+>>>>>>> Stashed changes
 
 app = QApplication([])
 
