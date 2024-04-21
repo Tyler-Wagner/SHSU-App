032196530e4ebb6ad3cc1104544d3de162522315
Tyler Wagner <tylerrwagner38@gmail.com>
2024-04-21 14:49:21 -0500

    Cleaning up backend
    Cleaned up the back end and implemented the snort rules parser as well as updated the readme

--- Backend/config.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-# I need to work on this at some point in the near future
-# all this will do is allow us to read and create config files
-# specific to each user
-
-class Config:
-    def __init__(self) -> None:
-        pass
